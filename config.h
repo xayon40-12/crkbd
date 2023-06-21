3f09ad348e8414ed6a1f17400792c2a4e8068a56
xayon40-12 <touroux.nathan@gmail.com>
2023-06-21 11:18:51 +0900

    update

--- config.h
+++ config.h
@@ -24,7 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define IGNORE_MOD_TAP_INTERRUPT
 //#define TAPPING_TERM 333
 
 #define MASTER_LEFT
